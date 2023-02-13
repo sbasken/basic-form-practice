ead6a4973252aca89cdaaf6ace9703014b6b43df
Saki <sbasken@gmail.com>
2023-02-13 11:40:14 -0800

    create form and button eventListeners

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log("I'm connected!")
+const noteForm = document.querySelector("#note-form")
+const noteList = document.querySelector("#note-list")
+const increaseNumForm = document.querySelector("#increase-value-form")
+const likeButton = document.querySelector("#like-button")
+const dislikeButton = document.querySelector("#dislike-button")
+const likes = document.querySelector("#like-count")
+
+noteForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const li = document.createElement('li');
+    li.textContent = e.target["new-note"].value;
+
+    noteList.append(li)
+})
+
+increaseNumForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let number = parseInt(document.querySelector("#value").textContent)
+    number += parseInt(e.target["new-value"].value)
+    document.querySelector("#value").textContent = number
+})
+
+likeButton.addEventListener("click", ()=>{
+    let num = parseInt(likes.textContent)
+    num += 1
+    likes.textContent = num
+})
+
+dislikeButton.addEventListener("click", ()=> {
+    if (likes.textContent <= 1) {
+        likes.textContent = 0
+    } else {
+        let num = parseInt(likes.textContent)
+        num -= 1
+        likes.textContent = num
+    }
+})
+
